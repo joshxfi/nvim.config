0a37442a1326edae7d0eb98c5d4828e3cb060e53
Josh Daniel <joshxfi.dev@gmail.com>
2024-01-19 12:31:01 +0800

    chore: update focused file on nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,16 +1,19 @@
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+    update_focused_file = {
+        enable = true,
+    }
 })
 
 vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
