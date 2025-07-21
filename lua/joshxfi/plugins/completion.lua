10b662de1635b3ac3bbcbba87c753c2e1615f97e
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-07-21 14:52:35 +0800

    chore: update completion opts

--- lua/joshxfi/plugins/completion.lua
+++ lua/joshxfi/plugins/completion.lua
@@ -55,7 +55,10 @@ return {
 				-- <c-k>: Toggle signature help
 				--
 				-- See :h blink-cmp-config-keymap for defining your own keymap
-				preset = "default",
+				preset = "enter",
+
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<Tab>"] = { "select_next", "fallback" },
 
 				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
