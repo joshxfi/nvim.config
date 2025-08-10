92710818b8df4d00ccc0c13e80565127e2a807d9
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2025-08-10 11:13:08 +0800

    chore: extend html to jsx

--- lua/joshxfi/plugins/cmp.lua
+++ lua/joshxfi/plugins/cmp.lua
@@ -26,6 +26,8 @@ return {
 						"rafamadriz/friendly-snippets",
 						config = function()
 							require("luasnip.loaders.from_vscode").lazy_load()
+							require("luasnip").filetype_extend("typescriptreact", { "html" })
+							require("luasnip").filetype_extend("javascriptreact", { "html" })
 						end,
 					},
 				},
